d657258bcabfa49d308dd69ee11cf775f33f74a9
Kevin Rosa <kevin_rosa@uri.edu>
2020-06-16 08:16:25 -0400

    Loop through multiple times. Include date string in output filename

--- script1.m
+++ script1.m
@@ -5,6 +5,13 @@ addpath(genpath('~/repos/ROMS-Scripts'))
 xi = 420:780;
 eta = 550:1030;
 
-fi_old = '/users/krosa1/projects/osom_2018/Output/osom_2018_400_his_0010.nc';
-fi_new = sprintf('%s_xi%i-%i_eta%i-%i_%s','/users/krosa1/projects/osom_2018/Output/osom_2018_400_his',xi(1),xi(end),eta(1),eta(end),'0010.nc');
-make_smaller_roms_file(fi_old, fi_new, xi, eta)
+run_code = 'osom_2018_400';
+t_inds = 10:40;
+DIR = '~/projects/osom_2018/Output/';
+
+for t = t_inds
+    fi_old = fullfile(DIR, sprintf('%s_his_%04i.nc', run_code, t));
+    time = nctime(fi_old,2,1);
+    fi_new = fullfile(DIR, sprintf('osom_%s_xi%i-%i_eta%i-%i.nc',datestr(time,'yyyymmdd'),xi(1),xi(end),eta(1),eta(end)));
+    make_smaller_roms_file(fi_old, fi_new, xi, eta)
+end
